7f2b05d328afb2993c6f9ca1df2c6815a274f8da
Shaofeng Jiang <sf.jiang.ca@gmail.com>
2018-07-07 14:20:29 -0400

    Unfinished refractor
    Add cmake and gtest

---
+++ src/31. Next Permutation.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+
+ifstream fin("infile.txt");
+ofstream fou("oufile.txt");
+
+void nextPermutation(vector<int> &nums) {
+    
+}
---
+++ test/test31/test31.cpp
@@ -0,0 +1,16 @@
+#include "test31.h"
+#include <iostream>
+using namespace std;
+
+Test31::Test31(){};
+
+Test31::~Test31(){};
+
+void Test31::SetUp(){};
+
+void Test31::TearDown(){};
+
+TEST_F(Test31, EmptyGraph)
+{
+
+}
